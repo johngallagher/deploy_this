0a5194c81f525d0c7b92592cfd751678b7e30142
John Gallagher <john@synapticmishap.co.uk>
2018-05-02 12:54:07 +0100

    Added tests

---
+++ lib/toolbelt/deploy.rb
@@ -0,0 +1,73 @@
+class Shell
+  def self.execute(command)
+    %x{#{command}}
+  end
+
+  def self.success?(command)
+    system(command)
+  end
+
+  def self.popen(command, &block)
+    IO.popen(command) { block.yield }
+  end
+end
+
+module Toolbelt
+  class Deploy
+    def initialize environment, server_name: Rails.root.basename.to_s, shell: Shell, stdout: STDOUT
+      @environment = environment
+      @server_name = server_name
+      @shell = shell
+      @stdout = stdout
+    end
+
+    def call
+      if cloud_66_toolbelt_installed?
+        @stdout.puts "Pushing #{current_branch}..."
+        push_branch
+        @stdout.puts ''
+
+        @stdout.puts 'Redeploying app...'
+        redeploy
+      else
+        @stdout.puts 'Cloud 66 toolbelt not found.'
+        @stdout.puts ''
+        @stdout.puts 'Install with:'
+        @stdout.puts 'brew install cloud66/tap/c66cx'
+        @stdout.puts ''
+      end
+    end
+
+    def current_branch
+      @current_branch ||= @shell.execute("echo $(git rev-parse --abbrev-ref HEAD)").chomp
+    end
+
+    def cloud_66_toolbelt_installed?
+      @shell.success?("command -v cx > /dev/null")
+    end
+
+    def stack_and_environment_switches
+      "--stack=#{@server_name} --environment=#{@environment}"
+    end
+
+    def current_branch_on_server
+      @shell.execute("cx settings list #{stack_and_environment_switches} git.branch").chomp.split(' ').select(&:present?).second
+    end
+
+    def change_deploy_branch branch
+      @shell.execute("cx settings set git.branch #{branch} #{stack_and_environment_switches}")
+    end
+
+    def push_branch
+      @shell.execute("git push --set-upstream origin #{current_branch}")
+    end
+
+    def redeploy
+      @shell.popen("cx redeploy --git-ref=#{current_branch} #{stack_and_environment_switches} --listen") do |io|
+        while (line = io.gets)
+          @stdout.puts line
+        end
+      end
+    end
+  end
+end
---
+++ spec/toolbelt/deploy_spec.rb
@@ -0,0 +1,48 @@
+require 'toolbelt/deploy'
+
+describe Toolbelt::Deploy do
+  describe '#call' do
+    let(:environment) { "staging" }
+    let(:server_name) { "server name" }
+    let(:current_branch) { "some-branch" }
+
+    subject { described_class.new(environment, server_name: server_name, shell: FakeShell, stdout: StringIO.new) }
+
+    before do
+      expect(FakeShell).to receive(:success?).with("command -v cx > /dev/null").and_return(cx_tool_installed)
+    end
+
+    context 'when cx tool is installed' do
+    let(:cx_tool_installed) { true }
+    it 'deploys' do
+      expect_shell_command("git push --set-upstream origin #{current_branch}")
+      expect_shell_command("echo $(git rev-parse --abbrev-ref HEAD)", and_return: current_branch)
+      expect_shell_command_streamed("cx redeploy --git-ref=#{current_branch} --stack=#{server_name} --environment=#{environment} --listen")
+
+      subject.call
+    end
+    end
+  end
+
+  private
+
+  def expect_shell_command(command, and_return: nil)
+    expect(FakeShell).to receive(:execute).with(command).and_return(and_return)
+  end
+
+  def expect_shell_command_streamed(command)
+    expect(FakeShell).to receive(:popen).with(command)
+  end
+end
+
+class FakeShell
+  def self.execute(command)
+  end
+
+  def self.success?(command)
+  end
+
+  def self.popen(command, &block)
+  end
+end
+
